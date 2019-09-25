acb7f852b948359f803cd2bc85b40728130a4d4a
sandeep <sandeep@ashmar.in>
2019-09-25 10:00:46 +0530

    add

---
+++ minmaxlib/src/main/java/com/example/minmaxlib/Sorting.java
@@ -0,0 +1,11 @@
+package com.example.minmaxlib;
+
+import java.util.Arrays;
+
+public class Sorting {
+  public String show(int[] a){
+
+        Arrays.sort(a);
+       return (Arrays.toString(a));
+    }
+}
